6bc4ad05cc7f11b9c93f7d61354f65c9a3861219
Andrey Konyaev <a.konyaev@hostcomm.ru>
2016-05-29 19:02:28 +0300

    fi

--- src/main/java/ru/ostrovok/test/App.java
+++ src/main/java/ru/ostrovok/test/App.java
@@ -1,6 +1,14 @@
 package ru.ostrovok.test;
 
+import org.apache.flink.streaming.api.datastream.DataStreamSink;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer08;
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+
 import java.util.Properties;
+
+
+
 
 public class App 
 {
@@ -10,7 +18,9 @@ public class App
         Properties properties = new Properties();
         properties.setProperty("zookeeper.connect", "akonyaev-test-1.dev.hc.ru:2181");
         properties.setProperty("group.id", "test");
-        DataStream<String> stream = env.addSource(new FlinkKafkaConsumer08<>("test-topic", new SimpleStringSchema(), properties)).print();
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.enableCheckpointing(5000);
+        DataStreamSink<String> stream = env.addSource(new FlinkKafkaConsumer08<>("test-topic", new SimpleStringSchema(), properties)).print();
 
     }
 }
